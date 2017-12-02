0fb8e960caf1ec1fc50ef09f4e2ac7e9d15507c0
puruj <puruelectron@gmail.com>
2017-12-02 15:00:11 -0500

    Exe file created

---
+++ Assets/Let's Try Shooter/Scripts/CompletedScripts/Debugger.js
@@ -0,0 +1,9 @@
+﻿import System;
+import System.IO;
+ 
+static function Log (debugMsg : String) {
+      var path : String = Application.dataPath+"/debugLog.txt";
+      var sw : StreamWriter = new StreamWriter(path, true);
+      sw.WriteLine(debugMsg+"\n");
+      sw.Close();
+}  
